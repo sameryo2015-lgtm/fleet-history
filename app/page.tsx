240ef04b91c82b951b9ef9d21bd0e0e4c190dd66
sameryo2015-lgtm <sameryo2015@gmail.com>
2026-07-14 13:28:27 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", maxWidth: "900px", margin: "0 auto" }}>
+      <h1>Fleet History</h1>
+
+      <p>艦艇を入口として歴史を学ぶサイト</p>
+
+      <hr />
+
+      <h2>研究ノート</h2>
+
+      <ul>
+        <li>HMS Ark Royal (91)</li>
+        <li>戦艦大和</li>
+        <li>Bismarck</li>
+      </ul>
+    </main>
+  );
+}
